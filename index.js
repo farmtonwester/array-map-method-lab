072c24cfc342fd830b6993ee35170244f6a9027f
Weston Farmer <wesfarmer11@gmail.com>
2024-01-11 09:21:42 -0600

    titleCased() returns an array of titledcased strings

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased() {
+  return tutorials.map((title) => {
+    const words = title.split(' ');
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1); 
+    })
+    return capitalizedWords.join(' ');
+  })
+};
+
+console.log(titleCased());
